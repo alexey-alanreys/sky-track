de26da7cf43205f67a9ef501adf73350acfef4be
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-12-18 18:49:51 +1000

    Setup Prettier, TailwindCSS, and React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import { Home } from './Home'
+
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
 )
