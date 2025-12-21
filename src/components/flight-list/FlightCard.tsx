080cb40d2b92c81895d994c53b92f804b2014aa3
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-12-21 16:52:02 +1000

    Add ProgressBar component

--- src/components/flight-list/FlightCard.tsx
+++ src/components/flight-list/FlightCard.tsx
@@ -1,5 +1,7 @@
 import { useSearchParams } from 'react-router';
 
+import { ProgressBar } from '@/components/custom-ui/ProgressBar';
+
 import { cn } from '@/utils/cn';
 
 import type { IFlight } from '@/types/flight.types';
@@ -19,21 +21,22 @@ export const FlightCard = ({ flight }: Props) => {
 
 	return (
 		<div
+			// TODO: add this gradient to tailwind css variables
 			className={cn(
 				'group relative w-full rounded-lg p-0.5 transition-colors ease-in',
 				isActive
-					? 'bg-linear-to-r from-rose-500 to-orange-400'
+					? 'g-linear-to-r from-rose-500 to-orange-400'
 					: 'bg-transparent'
 			)}
 		>
 			<FlightCardActions flightId={flight.id} />
 			<button
-				className='block h-full w-full rounded-lg bg-neutral-900 p-5'
-				onClick={() =>
+				onClick={() => {
 					setSearchParams({
 						[QUERY_PARAM_FLIGHT]: flight.id
-					})
-				}
+					});
+				}}
+				className={cn('block h-full w-full rounded-lg bg-neutral-900 p-5')}
 			>
 				<div className='mb-7 flex items-center justify-between'>
 					<div className='flex items-center gap-3'>
@@ -42,24 +45,26 @@ export const FlightCard = ({ flight }: Props) => {
 							alt={flight.airline.name}
 							width={40}
 							height={40}
-							className='rounded-full bg-white p-0.5'
+							className='rounded-full bg-white'
 						/>
 						<span>{flight.id}</span>
 					</div>
 					<div>
-						<span className='rounded-xl bg-neutral-800 px-1.5 py-0.5'>
+						<span className='rounded-xl bg-neutral-800 px-2 py-1'>
 							{flight.aircraftReg}
 						</span>
 					</div>
 				</div>
 
-				<div className='flex items-center justify-between'>
-					<div className='w-50 space-y-0.5 text-left'>
+				<div className='grid grid-cols-[1fr_5fr_1fr] items-end gap-4'>
+					<div className='space-y-0.5 text-left'>
 						<div>{flight.from.city}</div>
 						<div className='text-3xl font-semibold'>{flight.from.code}</div>
 					</div>
 
-					<div>{/* PROGRESS BAR */}</div>
+					<div className='mb-4'>
+						<ProgressBar percentage={flight.progress} />
+					</div>
 
 					<div>
 						<div>{flight.to.city}</div>
